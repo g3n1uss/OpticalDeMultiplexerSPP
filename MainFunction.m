bc17f36134b7f5095ebdfb7527478ddb1edd7e25
g3n1uss <g3n1uss@gmail.com>
2016-01-21 14:56:19 -0500

    names of variables are made more clear, added many comments

--- MainFunction.m
+++ MainFunction.m
@@ -1,7 +1,17 @@
-function [xifinal]=MainFunction(omega_p,eta,lambda_res,theta_res,r,fluxes)
+function [imped_grat_final]=MainFunction(plasma_freq,absorp_coef,wavelength_res,angle_res,order_res,energy_fluxes)
 %MAINFUNCTION This function computes properties of the grating realizing
-%the desired energy distribution and plots some data
-%   Detailed explanation goes here
+%the desired energy distribution and plots the resulting configuration and
+%energy spectra
+%   Output: modulated impedance of the grating
+%   Input: characteristics of the material such as
+%       - plasma_freq - plasma frequency (can be found for any material)
+%       - absorp_coef - coefficient of absoption (can be found for any material)
+%       - wavelength_res - wavelength on the incident beam
+%       - angle_res - incident angle
+%       - order_res - the number of diffracted spectra in which we want to
+%       have a resonance
+%       - energy fluxes - the desired energy distribution between outgoing
+%       beams
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -10,16 +20,16 @@ function [xifinal]=MainFunction(omega_p,eta,lambda_res,theta_res,r,fluxes)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % create object Surface material containing all the characteristics of the
 % material at the resonant point
-goldsurface=SurfaceMaterial(lambda_res,omega_p,eta);
+surface=SurfaceMaterial(wavelength_res,plasma_freq,absorp_coef);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Calculate the period of the impedance grating
-% sin(th_res)+r lambda_res/d_res=sqrt(1+Im(xi)^2)
+% Calculate the grat_period of the impedance grating
+% sin(th_res)+order_res wavelength_res/d_res=sqrt(1+Im(xi)^2)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-period=r*lambda_res/(sqrt(1+imag(goldsurface.xi0)^2)-sin(theta_res*pi/180));
+grat_period=order_res*wavelength_res/(sqrt(1+imag(surface.xi0)^2)-sin(angle_res*pi/180));
 % The following spectra will be outgoing (Nn,...,Nm)
-Np=floor((1-sin(theta_res*pi/180))*period/lambda_res);
-Nm=-floor((1+sin(theta_res*pi/180))*period/lambda_res);
+Np=floor((1-sin(angle_res*pi/180))*grat_period/wavelength_res);
+Nm=-floor((1+sin(angle_res*pi/180))*grat_period/wavelength_res);
 % the number of outgoing spectra is
 out_num=Np-Nm+1;
 
@@ -37,12 +47,12 @@ if nargin<6
         s(i)=en;
     end
 else
-    s=fluxes;
+    s=energy_fluxes;
 end
     
 
 % find tangential wavevectors "beta" for diffracted spectra
-diffSpectra=DiffAngles(theta_res,lambda_res,period);
+diffSpectra=DiffAngles(angle_res,wavelength_res,grat_period);
 betas=diffSpectra.beta;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -53,20 +63,21 @@ betas=diffSpectra.beta;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % At first we have only the amplitudes of various modes of the grating
 out_num_tmp=20;
-uinit=AnalyticSolution(out_num_tmp,goldsurface.xi0,s,betas,r).u;
+uinit=AnalyticSolution(out_num_tmp,surface.xi0,s,betas,order_res).u;
 psiinit=zeros(1,20);
 xianal=zeros(1,20);
+% the modulated impedance is xi_n=i u_n e^{i psi_n}
 xianal(:)=1i*uinit(:).*exp(1i*psiinit(:));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Define the correcting functional and then minimize it
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-func=Functional(goldsurface.xi0,betas,uinit,r,s);
+func=Functional(surface.xi0,betas,uinit,order_res,s);
 % minimize it
 funcmin=func.Minimize(); 
 % construct the resulting grating
-xifinal=zeros(1,20);
-xifinal(:)=1i*funcmin.ufinal(:).*exp(1i*funcmin.psifinal(:));
+imped_grat_final=zeros(1,20);
+imped_grat_final(:)=1i*funcmin.ufinal(:).*exp(1i*funcmin.psifinal(:));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Generate data to plot
@@ -74,15 +85,18 @@ xifinal(:)=1i*funcmin.ufinal(:).*exp(1i*funcmin.psifinal(:));
 
 % At first generate data for the initial grating obtained from the 
 % analytic solution
-dataGenInit=DataGenerator(r,lambda_res,theta_res,goldsurface,diffSpectra,xianal);
+dataGenInit=DataGenerator(order_res,wavelength_res,angle_res,surface,diffSpectra,xianal);
 % data for initial grating
 % along wavelength
+% As output we get energy fluxes "s_nonres_w", the square module of the 
+% resonance mode "abs_h_res2_w", the absoption "absorption_w"; all of them 
+% are caluclated at each wavelength from "wavelength_w"
 [s_nonres_w,abs_h_res2_w,absorption_w,wavelength_w,~]=dataGenInit.getDataWavelength(0);
 % along angle
 [s_nonres_a,abs_h_res2_a,absorption_a,~,thetas_a]=dataGenInit.getDataAngle(0);
 
 % data for final grating
-dataGenFinal=DataGenerator(r,lambda_res,theta_res,goldsurface,diffSpectra,xifinal);
+dataGenFinal=DataGenerator(order_res,wavelength_res,angle_res,surface,diffSpectra,imped_grat_final);
 % along wavelength
 [s_nonres_w_f,abs_h_res2_w_f,absorption_w_f,wavelength_w_f,~]=dataGenFinal.getDataWavelength(0);
 % along angle
@@ -94,25 +108,26 @@ dataGenFinal=DataGenerator(r,lambda_res,theta_res,goldsurface,diffSpectra,xifina
 % set the line width
 linewidth=2;
 % plot the grating
+% set step for plotting (could be arbitrary)
 t=0:.01:1;
-% initial grating
-grat_i=BuildGrating(uinit,psiinit,goldsurface.xi0,t);
+% construct the initial grating
+grat_i=BuildGrating(uinit,psiinit,surface.xi0,t);
 % label for initial grating
 labelUinit=GenerateLabel('u^i',uinit);
-labelXi=strcat(labelUinit,', \xi_0^{"}=',num2str(imag(goldsurface.xi0)));
+labelXi=strcat(labelUinit,', \xi_0^{"}=',num2str(imag(surface.xi0)));
 % final grating
-grat_f=BuildGrating(funcmin.ufinal,funcmin.psifinal,goldsurface.xi0,t);
+grat_f=BuildGrating(funcmin.ufinal,funcmin.psifinal,surface.xi0,t);
 % label for final grating
 labelUfinal=GenerateLabel('u^f',funcmin.ufinal);
 labelPsifinal=GenerateLabel('psi^f',funcmin.psifinal*180/pi);
 labelXi_f=strcat(labelUfinal, labelPsifinal,', \xi_0^{"}=',...
-    num2str(imag(goldsurface.xi0)));
+    num2str(imag(surface.xi0)));
 % set the display window to fullscreen
 figure('units','normalized','outerposition',[0 0 1 1]);
-% draw grating
+% draw the initial and corrected gratings
 subplot(3,2,[1,2]),plot(t,grat_i,'--k',t,grat_f,'k','LineWidth',linewidth),text(0.3,...
-    imag(goldsurface.xi0)/2,labelXi),...
-    text(0.3,imag(goldsurface.xi0)/1.5,labelXi_f),xlabel('x/d'),...
+    imag(surface.xi0)/2,labelXi),...
+    text(0.3,imag(surface.xi0)/1.5,labelXi_f),xlabel('x/d'),...
     ylabel('\xi^{"}'),legend('Grating from analytical solution','Corrected grating'),...
     title('Impedance of the grating')
 
@@ -124,10 +139,10 @@ subplot(3,2,3), plot(wavelength_w(:),abs_h_res2_w(:),'--k',wavelength_w_f(:),...
     title('Resonance amplitude')
 
 
-% plot the fluxes obtained from the analitycal solution
-% label for the fluxes
+% plot the energy_fluxes obtained from the analitycal solution
+% label for the energy_fluxes
 labelS=GenerateLabel('s',s);
-% label for legend
+% label for the legend
 legendlabel=cell(1,2*(out_num+1));
 ind=1;
 legendlabel{ind}='A^i';
@@ -158,7 +173,7 @@ subplot(3,2,5), plot(thetas_a(:),abs_h_res2_a(:),'--k',thetas_a_f(:),...
     legend('Analytical solution','Corrected solution'),...
     title('Resonance amplitude')
 
-% plot the corrected fluxes
+% plot the corrected energy_fluxes
 subplot(3,2,6),
 plot(thetas_a(:),absorption_a(:),'--','LineWidth',linewidth);
 hold on
